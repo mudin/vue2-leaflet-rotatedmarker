7c98f61cdcd1ce2efbc05d98e338c4739d1d72fc
mudin <muhridin@next-aeon.com>
2019-05-04 00:55:05 +0900

    version 1.0.7 release

--- example.vue
+++ example.vue
@@ -13,9 +13,8 @@
 <script>
   import { Icon, LatLng } from 'leaflet'
   import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
-  import Vue2LeafletRotatedMarker from './Vue2LeafletRotatedMarker'
+  import Vue2LeafletRotatedMarker from './dist/Vue2LeafletRotatedMarker.js'
   import iconUrl from 'leaflet/dist/images/marker-icon.png'
-  import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 
   function rand(n) {
     let max = n + 0.1
@@ -33,24 +32,27 @@
     methods: {
       handleMarkerClick(l){
         l.yaw+=20;
+      },
+      frame() {
+        this.locations[0].yaw+=2;
+        this.locations[1].yaw+=2;
+        this.locations[2].yaw+=2;
       }
     },
     data () {
-      let locations = []
+      this.locations = []
       for (let i = 0; i < 10; i++) {
-        locations.push({
+        this.locations.push({
           id: i,
-          yaw:0,
+          yaw: Math.random() * 360 - 180,
           latlng: new LatLng(rand(-34.9205), rand(-57.953646)),
           text: 'Hola ' + i
         })
       }
-      let icon = new Icon({iconUrl, shadowUrl})
+      let icon = new Icon({iconUrl})
       return {
-        locations,
+        locations:this.locations,
         icon,
-        clusterOptions: {},
-        rotationAngle:45,
         initialLocation: new LatLng(-34.9205, -57.953646)
       }
     },
@@ -61,6 +63,7 @@
           this.clusterOptions = { disableClusteringAtZoom: 11 }
         });
       }, 5000);
+      this.intervalId = setInterval(this.frame, 100);
     }
   }
 </script>
